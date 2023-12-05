e8b96227b9d5cd688656f278cc5092b5b534ea42
Brandon Wellsbury <90425294+wellsbury@users.noreply.github.com>
2023-12-05 15:31:12 -0500

    Delete about_me.py

--- about_me.py
+++
@@ -1,9 +0,0 @@
-def brandon_config():
-    return {
-        "name": "Brandon Wellsbury",
-        "location": "Toronto, Canada",
-        "currently": ["Data Engineer", "BI Developer", "Mentor"],
-        "hobbies": ["Hockey", "Snowboarding", "Golf", "Guitar", "Reading"]
-        "previously": ["Director of Marketing Technology", "Product & Project Manager", "Greenskeeper"],
-        "languages": ["Python", "SQL", "LookML", "MQL"],
-    }
